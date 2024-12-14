3acf1730e786594c1caa2da3f664eb6f2725d8b1
Pro100Dima1 <dima.sinkin@yandex.ru>
2024-12-14 17:45:45 +0300

    optimize API test

--- src/test/java/org/example/UnicornApi.java
+++ src/test/java/org/example/UnicornApi.java
@@ -5,63 +5,47 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.example.api.UnicornRequests;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasKey;
 
 public class UnicornApi {
+    @BeforeAll
+    public static void setUpTests(){
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter()); // Фильтра рест ашура для того что б в консоли видеть что кладется в запрос и ответ
+        RestAssured.baseURI = "https://crudcrud.com/api/d53814d6405b4b148603c1bf42e66af1"; // Базовый урл. Он автоматически подставляется в методах post, get итд
+        // Принцип программирования DRY Do Not Repeat Yourself: Если пишешь один и тот же код дважды, он должен быть вынесен в метод
+    }
+
     @Test
     public void userShouldBeAbleCreateUnicorn(){
-        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter()); // Фильтра рест ашура для того что б в консоли видеть что кладется в запрос и ответ
-        // given - when - then  BDD
-        given()
-                .body("{\n" +
-                        "  \"name\": \"AppleJack\",\n" +
-                        "  \"tailColor\": \"orange\"\n" +
-                        "}")
-                .contentType(ContentType.JSON)
-        .when()
-                .post("https://crudcrud.com/api/d53814d6405b4b148603c1bf42e66af1/unicorn")
-        .then()
-                .assertThat()
-                .statusCode(201)
-                .body("$", hasKey("_id"));
-
+         UnicornRequests.createUnicorn("{\n" +
+                "  \"name\": \"AppleJack\",\n" +
+                "  \"tailColor\": \"orange\"\n" +
+                "}");
     }
+
     @Test
     public void userShouldBeAbleDeleteExistingUnicorn(){
         //Принципы разработки API тестов:
         //1) Атомарность
         //2) Тест сам себе готовит данные
 
-        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter()); // Фильтра рест ашура для того что б в консоли видеть что кладется в запрос и ответ
        // Шаг 1 Создание единорога
-        String id = given()
-                .body("{\n" +
-                        "  \"name\": \"AppleJack\",\n" +
-                        "  \"tailColor\": \"orange\"\n" +
-                        "}")
-                .contentType(ContentType.JSON)
-        .when()
-                .post("https://crudcrud.com/api/d53814d6405b4b148603c1bf42e66af1/unicorn")
-        .then()
-                .assertThat()
-                .statusCode(201)
-                .body("$", hasKey("_id"))
-        .extract()
-                .path("_id");  // extract - вытаскивает значение из указанного поля
+        String id = UnicornRequests.createUnicorn("{\n" +
+                "  \"name\": \"AppleJack\",\n" +
+                "  \"tailColor\": \"orange\"\n" +
+                "}");
 
         //Шаг 2 Удаление единорога
-        given()
-                .delete("https://crudcrud.com/api/d53814d6405b4b148603c1bf42e66af1/unicorn/" + id)
-        .then()
-                .assertThat()
-                .statusCode(200);
+        UnicornRequests.deleteUnicorn(id);
 
         //Щаг 3 Проверка что единорог удалился
         given()
-                .get("https://crudcrud.com/api/d53814d6405b4b148603c1bf42e66af1/unicorn/" + id)
+                .get("/unicorn/" + id)
         .then()
                 .assertThat()
                 .statusCode(HttpStatus.SC_NOT_FOUND);
---
+++ src/test/java/org/example/api/UnicornRequests.java
@@ -0,0 +1,31 @@
+package org.example.api;
+
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+
+public class UnicornRequests {
+    public static String createUnicorn(String body){
+        // given - when - then  BDD
+        return given()
+                .body(body)
+                .contentType(ContentType.JSON)
+        .when()
+                .post("/unicorn")
+        .then()
+                .assertThat()
+                .statusCode(201)
+                .body("$", hasKey("_id"))
+        .extract()
+                .path("_id");  // extract - вытаскивает значение из указанного поля
+    }
+
+    public static void deleteUnicorn(String id){
+        given()
+                .delete("/unicorn/" + id)
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+}
